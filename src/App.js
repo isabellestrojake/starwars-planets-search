8ac9e1987fb8a9b7e1558a875fec032e47285187
Isabelle Strojake <strojake@gmail.com>
2022-10-20 13:11:09 -0300

    config remove filters

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Table from './components/Table';
 import NameFilter from './components/NameFilter';
 import NumberFilter from './components/NumberFilter';
+import FiltersRender from './components/FiltersRender';
 import Provider from './context/provider';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Provider>
       <NameFilter />
       <NumberFilter />
+      <FiltersRender />
       <Table />
     </Provider>
   );
